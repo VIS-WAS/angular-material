9f7d0289b48733f40384c8191589dff7de9a59c0
VIS-WAS <vishwas.ms8830@gmail.com>
2024-07-30 22:45:39 +0530

    input and select material

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  exports: [MatFormFieldModule, MatInputModule, MatSelectModule],
+})
+export class MaterialModule {}
