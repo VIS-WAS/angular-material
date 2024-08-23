3b6b835d81f07598b63316250c6f7b851f6c597c
VIS-WAS <vishwas.ms8830@gmail.com>
2024-08-23 21:45:59 +0530

    reactive form using FormBuilder

--- src/app/form-design/form-design.component.ts
+++ src/app/form-design/form-design.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 interface Country {
   value: string;
@@ -10,8 +15,8 @@ interface Country {
   templateUrl: './form-design.component.html',
   styleUrls: ['./form-design.component.css'],
 })
-export class FormDesignComponent {
-  forms!: FormGroup;
+export class FormDesignComponent implements OnInit {
+  // forms!: FormGroup;
 
   countries: Country[] = [
     { value: 'ind', viewValue: 'India' },
@@ -24,19 +29,44 @@ export class FormDesignComponent {
   favoriteSeason!: string;
   seasons: string[] = ['Winter', 'Spring', 'Summer', 'Autumn'];
 
-  ngOnInit() {
-    this.forms = new FormGroup({
-      fistname: new FormControl(null, Validators.required),
-      email: new FormControl(null, [Validators.required, Validators.email]),
-      phone: new FormControl(null),
-      country: new FormControl(null),
-      term: new FormControl(null),
-      address: new FormControl(null),
-      season: new FormControl(null),
+  // ngOnInit() {
+  //   this.forms = new FormGroup({
+  //     fistname: new FormControl(null, Validators.required),
+  //     email: new FormControl(null, [Validators.required, Validators.email]),
+  //     phone: new FormControl(null),
+  //     country: new FormControl(null),
+  //     term: new FormControl(null),
+  //     address: new FormControl(null),
+  //     season: new FormControl(null),
+  //   });
+  // }
+
+  constructor(private builder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.forms.patchValue({
+      phone: '9878766767',
     });
   }
 
+  forms = this.builder.group({
+    firstname: this.builder.control('', Validators.required),
+    email: this.builder.control('', [(Validators.required, Validators.email)]),
+    phone: this.builder.control(''),
+    country: this.builder.control(''),
+    term: this.builder.control(''),
+    address: this.builder.control(''),
+    season: this.builder.control('Winter'),
+    dob: this.builder.control(new Date()),
+    checked: this.builder.control(''),
+    inter: this.builder.control(true),
+  });
+
   formSaved() {
     console.log(this.forms);
+    this.clearForm();
+  }
+  clearForm() {
+    this.forms.reset();
   }
 }
